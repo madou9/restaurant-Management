00d1558e44bbd9dea3b80734986b49dd76fb7703
madou9 <issoufou.mamadou@gmail.com>
2024-10-26 21:32:28 +0200

    create and implement Nav page

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="nav">
+        <router-link to="/">Home</router-link>
+        <router-link to="Add">Add restaurant </router-link>
+        <router-link to="Update">Update restaurant</router-link>
+        <a v-on:click="logout" href="#">Logout</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+        methods: {
+            logout() {
+                localStorage.clear();
+                this.$router.push({ name: 'Login' });
+            }
+        }
+    }
+</script>
+
+<style>
+    .nav {
+        background-color: #333;
+        overflow: hidden;
+        width: 100%;              /* Full width */
+    }
+
+    .nav a {
+        float: left;
+        display: block;
+        color: #f2f2f2;
+        text-align: center;
+        font-size: 17px;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    .nav a:hover {
+        background-color: #ddd;
+        color: black;
+    }
+</style>
