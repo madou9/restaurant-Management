10a570ee64cf545ba63c643795c53ab69e75d16f
madou9 <issoufou.mamadou@gmail.com>
2024-10-26 21:31:34 +0200

    updated the Home page to show the user name after login or sign Up

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,26 @@
 <template>
+    <Header />
     <h1>
-        Hello user, Welcome to our restaurant website
+        Hello {{ name }}, Welcome to our Restaurant website
     </h1>
 </template>
 
 <script>
+import Header from './Header.vue';
 export default {
     name: 'Home',
+    data() {
+        return {
+            name: "",
+        };
+    },
+    components: {
+        Header
+    },
     mounted() {
         // Redirect to SignUp if no user info is found in localStorage
         let user = localStorage.getItem('user-info');
+        this.name = JSON.parse(user).name;
         if (!user) {  // Check if user-info does NOT exist
             this.$router.push({ name: 'SignUp' });
         }
