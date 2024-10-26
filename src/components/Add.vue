2e6003a34847749f13b7e27d5eb48993d30c980b
madou9 <issoufou.mamadou@gmail.com>
2024-10-26 22:39:35 +0200

    implemented Add function

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -3,6 +3,12 @@
     <h1>
         Hello User, Welcome to Add Restaurant website
     </h1>
+    <form class="add">
+        <input type="text" v-model="restaurant.name" placeholder="Enter Name" />
+        <input type="text" v-model="restaurant.contact" placeholder="Enter Contact" />
+        <input type="text" v-model="restaurant.address" placeholder="Enter Address" />
+        <button type="button" v-on:click="addRestaurant">Add new Restaurant</button>
+    </form>
 </template>
 
 <script>
@@ -11,6 +17,21 @@ export default {
     name: 'Add',
     components: {
         Header
+    },
+    data ()
+    {
+        return {
+            restaurant:{
+                name: '',
+                contact: '',
+                address: ''
+            },
+        }
+    },
+    methods:{
+        addRestaurant(){
+            console.warn(this.restaurant);
+        }
     },
     mounted() {
         // Redirect to SignUp if no user info is found in localStorage
