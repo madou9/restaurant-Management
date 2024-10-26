0b12be5b3aa27c528e46eab3b335f59586f9b6d0
madou9 <issoufou.mamadou@gmail.com>
2024-10-26 21:32:43 +0200

    create and implement Update page

---
+++ src/components/Update.vue
@@ -0,0 +1,23 @@
+<template>
+    <Header />
+    <h1>
+        Hello User, Welcome to update Restaurant website
+    </h1>
+</template>
+
+<script>
+import Header from './Header.vue';
+export default {
+    name: 'Update',
+    components: {
+        Header
+    },
+    mounted() {
+        // Redirect to SignUp if no user info is found in localStorage
+        let user = localStorage.getItem('user-info');
+        if (!user) {  // Check if user-info does NOT exist
+            this.$router.push({ name: 'SignUp' });
+        }
+    },
+}
+</script>
