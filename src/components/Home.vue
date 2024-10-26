92045c2c12a26dd4c39b9fdcc960fd24afb8ad26
madou9 <issoufou.mamadou@gmail.com>
2024-10-26 17:57:21 +0200

    updated

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,14 +5,14 @@
 </template>
 
 <script>
-    export default {
-        name: 'Home',
-        mounted() {
+export default {
+    name: 'Home',
+    mounted() {
+        // Redirect to SignUp if no user info is found in localStorage
         let user = localStorage.getItem('user-info');
-        if (user) {
-            this.$router.push({ name: '/SignUp' });
+        if (!user) {  // Check if user-info does NOT exist
+            this.$router.push({ name: 'SignUp' });
         }
     },
-    }
-
+}
 </script>
