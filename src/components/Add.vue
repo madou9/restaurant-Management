5cff45e56f6b829363823eb004915f4f8f35f7fc
madou9 <issoufou.mamadou@gmail.com>
2024-10-26 21:32:15 +0200

    create and implement Add page

---
+++ src/components/Add.vue
@@ -0,0 +1,23 @@
+<template>
+    <Header />
+    <h1>
+        Hello User, Welcome to Add Restaurant website
+    </h1>
+</template>
+
+<script>
+import Header from './Header.vue';
+export default {
+    name: 'Add',
+    components: {
+        Header
+    },
+    mounted() {
+        // Redirect to SignUp if no user info is found in localStorage
+        let user = localStorage.getItem('user-info');
+        if (!user) {  // Check if user-info does NOT exist
+            this.$router.push({ name: 'SignUp' });
+        }
+    },
+}
+</script>
