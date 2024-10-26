270ec7900800149ab6d612ef2a331d89a98fb9f9
madou9 <issoufou.mamadou@gmail.com>
2024-10-26 13:52:23 +0200

    implement Post request with axios package

--- restaurant-project/src/components/SignUp.vue
+++ restaurant-project/src/components/SignUp.vue
@@ -2,16 +2,39 @@
     <img alt="hero" class="logo" src="../assets/hero.png"/>
     <h1>Sign Up</h1>
     <div class="register">
-        <input type="text" placeholder="Enter Name"/>
-        <input type="text" placeholder="Enter Email"/>
-        <input type="password" placeholder="Enter Password"/>
-        <button>Sign Up</button>
+        <input type="text" v-model="name" placeholder="Enter Name"/>
+        <input type="text" v-model="email" placeholder="Enter Email"/>
+        <input type="password" v-model="password" placeholder="Enter Password"/>
+        <button v-on:click="signUp">Sign Up</button>
     </div>
 </template>
 <script>
+ import axios from 'axios';
 export default {
     name: 'SignUp',
+    data() {
+        return {
+            name: '',
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        async signUp() {
+            let result = await axios.post('http://localhost:3000/users', {
+                name: this.name,
+                email: this.email,
+                password: this.password
+            }); 
+            console.log(result);
+            if (result.status === 201) {
+                alert('User created successfully');
+                localStorage.setItem('user-info', JSON.stringify(result.data));
+            }
+        }
 }
+}
+
 </script>
 
 <style>
