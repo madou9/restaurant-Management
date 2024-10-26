00ab915c6b9a038ae9f9a3b8672fa6db13084382
madou9 <issoufou.mamadou@gmail.com>
2024-10-26 17:30:23 +0200

    redirect user to Sign up page if no register

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,13 @@
 
 <script>
     export default {
-        name: 'Home'
+        name: 'Home',
+        mounted() {
+        let user = localStorage.getItem('user-info');
+        if (user) {
+            this.$router.push({ name: '/SignUp' });
+        }
+    },
     }
 
 </script>
