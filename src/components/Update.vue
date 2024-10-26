f9cb34f8856e0b2ab924a3099a47327537b73397
madou9 <issoufou.mamadou@gmail.com>
2024-10-26 23:41:04 +0200

    updated

--- src/components/Update.vue
+++ src/components/Update.vue
@@ -3,21 +3,56 @@
     <h1>
         Hello User, Welcome to update Restaurant website
     </h1>
+    <form class="add">
+        <input type="text" v-model="restaurant.name" placeholder="Enter Name" />
+        <input type="text" v-model="restaurant.contact" placeholder="Enter Contact" />
+        <input type="text" v-model="restaurant.address" placeholder="Enter Address" />
+        <button type="button" v-on:click="updateRestaurant">Update new Restaurant</button>
+    </form>
 </template>
 
 <script>
+import axios from 'axios';
 import Header from './Header.vue';
 export default {
     name: 'Update',
     components: {
         Header
     },
-    mounted() {
+    data ()
+    {
+        return {
+            restaurant:{
+                name: '',
+                contact: '',
+                address: ''
+            },
+        }
+    },
+    methods:{
+       async updateRestaurant(){
+        let result = await axios.put('http://localhost:3000/restaurants/' + this.$route.params.id, {
+                name: this.restaurant.name,
+                contact: this.restaurant.contact,
+                address: this.restaurant.address
+            });
+            if (result.status === 200) {
+                this.restaurant.name = '';
+                this.restaurant.contact = '';
+                this.restaurant.address = '';
+                this.$router.push({ name: 'Home' });
+            }
+        }
+    },
+    async mounted() {
         // Redirect to SignUp if no user info is found in localStorage
         let user = localStorage.getItem('user-info');
         if (!user) {  // Check if user-info does NOT exist
             this.$router.push({ name: 'SignUp' });
         }
+        let result = await axios.get('http://localhost:3000/restaurants/' + this.$route.params.id);
+        this.restaurant = result.data;
+        // console.warn("result", result.data);
     },
 }
 </script>
